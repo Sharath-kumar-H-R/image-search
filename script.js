163dabcf11016197938e7246d0fad39ac3b073bb
Sharath Kumar H R <125861611+Sharath-kumar-H-R@users.noreply.github.com>
2023-09-26 09:21:08 +0530

    Update script.js
    update the access key

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // creating access key
-const accessKey = "GWOb4uTA1TsBUlkgHGyb7A3Xv7myK6-1xuGRy1yhTZk";
+const accessKey = "esOoiiyu3qu-0_ivIOJm9YmJHEuQXVbDZG8PimRfE7A";
 // access from html doc and store here
 const formE1 = document.querySelector("form");
 const inputE1 = document.getElementById("search-input");
@@ -10,10 +10,10 @@ const showMore = document.getElementById("show-more-button");
 let inputData = "";
 let page = 1;
 
- async function searchImages(){
+ async function searchImages() {
     inputData = inputE1.value;
     // created dynam7ic url based on input data
-    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&clint_id=${accessKey}`;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
 
 // fetch the images based on the query
  const response = await fetch(url);
@@ -30,37 +30,38 @@ let page = 1;
 
 //  now we have lot of data but qwe need to show in our pre4 deswign format
 //  so we use map to map the data results
- results.map((result) => {
+ results.map((result) =>{
     // create a container for resulst
-    const imageWrapper = document.createElement('div')
+    const imageWrapper = document.createElement("div")
+    imageWrapper.classList.add("search-result");
     // create a image element for image
-    const image = document.createElement('img');
+    const image = document.createElement("img");
     // for img source and attribute
     image.src=result.urls.small;
     image.alt = result.alt_description;
 
     // we have a in next in ou page, so we create that also
-    const imageLink = document.createElement('a')
+    const imageLink = document.createElement("a");
     //  fetch the li9nk and target set to blank(next_page) also text content
     imageLink.href = result.links.html;
     imageLink.target = "_blank";
     imageLink.textContent = result.alt_description;
     //  ue append child to push all resul;ts into our web page
-    imageWrapper.appendChild(image)
-    imageWrapper.appendChild(imageLink)
-    searchResults.appendChild(imageWrapper)
+    imageWrapper.appendChild(image);
+    imageWrapper.appendChild(imageLink);
+    searchResults.appendChild(imageWrapper);
 
 });
 
 // increase the page number
-page ++
+page++;
 if(page > 1){
     showMore.style.display = "block";
 }
 }
 
 formE1.addEventListener("submit", (event) =>{
-    event.preventDefault();
+    event.preventDefault() 
     page=1;
      searchImages();
 });
